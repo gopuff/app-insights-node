e077160694cfcb08b5b67df9e6aeea9a96bf6100
Casey Garland <casey@parsed.io>
2020-07-28 19:17:01 -0400

    use moment.unix

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ const client = appInsights.defaultClient;
 
 const file = `${process.env.APPCENTER_OUTPUT_DIRECTORY}/${process.env.APP_FILE}`;
 const fileStats = fs.statSync(file);
-const duration = moment().diff(moment(parseInt(process.env.APPCENTER_BUILD_ID)), 'seconds');
 const platform = process.env.APPCENTER_ANDROID_VARIANT ? 'android' : 'ios';
+const duration = moment().diff(moment.unix(parseInt(process.env.APPCENTER_BUILD_ID)), 'seconds');
+console.log(duration / 60);
 console.log(
   '-----------------------------------------------------------------------------------------------',
 );
